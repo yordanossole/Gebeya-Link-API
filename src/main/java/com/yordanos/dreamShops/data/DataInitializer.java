3e5c905fbdb8d4ae5a376e4bedda327e138607e9
yordanossole <yordanos.sole@gmail.com>
2025-04-30 19:02:46 +0300

    Configuration change for deployement purpos. Packaged using ./mvn too.

--- src/main/java/com/yordanos/dreamShops/data/DataInitializer.java
+++ src/main/java/com/yordanos/dreamShops/data/DataInitializer.java
@@ -1,72 +1,72 @@
-package com.yordanos.dreamShops.data;
-
-import com.yordanos.dreamShops.model.Role;
-import com.yordanos.dreamShops.model.User;
-import com.yordanos.dreamShops.repository.RoleRepository;
-import com.yordanos.dreamShops.repository.UserRepository;
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
-
-import java.util.Set;
-
-@Transactional
-@Component
-@RequiredArgsConstructor
-public class DataInitializer implements ApplicationListener<ApplicationReadyEvent> {
-    private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
-    private final PasswordEncoder passwordEncoder;
-
-    @Override
-    public void onApplicationEvent(ApplicationReadyEvent event) {
-        Set<String> defaultRoles = Set.of("ROLE_ADMIN", "ROLE_USER");
-        createDefaultRoleIfNotExits(defaultRoles);
-        createDefaultUserIfNotExits();
-        createDefaultAdminIfNotExits();
-    }
-
-    private void createDefaultUserIfNotExits() {
-        Role userRole = roleRepository.findByName("ROLE_USER").get(); // it had .get();
-        for (int i=1; i<=5; i++) {
-            String defaultEmail = "user" + i + "@email.com";
-            if (userRepository.existsByEmail(defaultEmail)) {
-                continue;
-            }
-            User user = new User();
-            user.setFirstName("The User");
-            user.setLastName("User" + i);
-            user.setEmail(defaultEmail);
-            user.setPassword(passwordEncoder.encode("password"));
-            user.setRoles(Set.of(userRole));
-            userRepository.save(user);
-            System.out.println("Default User " + i + " created.");
-        }
-    }
-    private void createDefaultAdminIfNotExits() {
-        Role adminRole = roleRepository.findByName("ROLE_ADMIN").get();
-        for (int i=1; i<=2; i++) {
-            String defaultEmail = "admin" + i + "@email.com";
-            if (userRepository.existsByEmail(defaultEmail)) {
-                continue;
-            }
-            User user = new User();
-            user.setFirstName("Admin");
-            user.setLastName("Admin" + i);
-            user.setEmail(defaultEmail);
-            user.setPassword(passwordEncoder.encode("password"));
-            user.setRoles(Set.of(adminRole));
-            userRepository.save(user);
-            System.out.println("Default admin User " + i + " created.");
-        }
-    }
-
-    private void createDefaultRoleIfNotExits(Set<String> roles) {
-        roles.stream()
-                .filter(role -> roleRepository.findByName(role).isEmpty())
-                .map(Role::new).forEach(roleRepository::save);
-    }
-}
+//package com.yordanos.dreamShops.data;
+//
+//import com.yordanos.dreamShops.model.Role;
+//import com.yordanos.dreamShops.model.User;
+//import com.yordanos.dreamShops.repository.RoleRepository;
+//import com.yordanos.dreamShops.repository.UserRepository;
+//import jakarta.transaction.Transactional;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.boot.context.event.ApplicationReadyEvent;
+//import org.springframework.context.ApplicationListener;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.Set;
+//
+//@Transactional
+//@Component
+//@RequiredArgsConstructor
+//public class DataInitializer implements ApplicationListener<ApplicationReadyEvent> {
+//    private final UserRepository userRepository;
+//    private final RoleRepository roleRepository;
+//    private final PasswordEncoder passwordEncoder;
+//
+//    @Override
+//    public void onApplicationEvent(ApplicationReadyEvent event) {
+//        Set<String> defaultRoles = Set.of("ROLE_ADMIN", "ROLE_USER");
+//        createDefaultRoleIfNotExits(defaultRoles);
+//        createDefaultUserIfNotExits();
+//        createDefaultAdminIfNotExits();
+//    }
+//
+//    private void createDefaultUserIfNotExits() {
+//        Role userRole = roleRepository.findByName("ROLE_USER").get(); // it had .get();
+//        for (int i=1; i<=5; i++) {
+//            String defaultEmail = "user" + i + "@email.com";
+//            if (userRepository.existsByEmail(defaultEmail)) {
+//                continue;
+//            }
+//            User user = new User();
+//            user.setFirstName("The User");
+//            user.setLastName("User" + i);
+//            user.setEmail(defaultEmail);
+//            user.setPassword(passwordEncoder.encode("password"));
+//            user.setRoles(Set.of(userRole));
+//            userRepository.save(user);
+//            System.out.println("Default User " + i + " created.");
+//        }
+//    }
+//    private void createDefaultAdminIfNotExits() {
+//        Role adminRole = roleRepository.findByName("ROLE_ADMIN").get();
+//        for (int i=1; i<=2; i++) {
+//            String defaultEmail = "admin" + i + "@email.com";
+//            if (userRepository.existsByEmail(defaultEmail)) {
+//                continue;
+//            }
+//            User user = new User();
+//            user.setFirstName("Admin");
+//            user.setLastName("Admin" + i);
+//            user.setEmail(defaultEmail);
+//            user.setPassword(passwordEncoder.encode("password"));
+//            user.setRoles(Set.of(adminRole));
+//            userRepository.save(user);
+//            System.out.println("Default admin User " + i + " created.");
+//        }
+//    }
+//
+//    private void createDefaultRoleIfNotExits(Set<String> roles) {
+//        roles.stream()
+//                .filter(role -> roleRepository.findByName(role).isEmpty())
+//                .map(Role::new).forEach(roleRepository::save);
+//    }
+//}
